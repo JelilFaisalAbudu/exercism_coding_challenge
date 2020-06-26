8bc4cb1deba56fb478d1ac3655e1b93cb8266be7
JelilAbuduFaisal <abudujelilfaisal@gmail.com>
2020-06-26 19:23:20 +0000

    Update challenges.rb

--- challenges.rb
+++ challenges.rb
@@ -1,3 +1,19 @@
+
+=begin
+Write your code for the 'Two Fer' exercise in this file. Make the tests in
+`two_fer_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/two-fer` directory.
+=end
+
+module TwoFer
+  def self.two_fer(name = 'you')
+    "One for #{name}, one for me."
+  end
+end
+
+
 =begin
 Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
 `resistor_color_duo_test.rb` pass.
