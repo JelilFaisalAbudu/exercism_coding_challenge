f8e5a5b74999280104cffb1d29fbb40fca240ae9
JelilAbuduFaisal <abudujelilfaisal@gmail.com>
2020-06-26 19:18:02 +0000

    Create challenges.rb
    Add ```ResistorColorDuo``` and its methods the file.

---
+++ challenges.rb
@@ -0,0 +1,25 @@
+=begin
+Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
+`resistor_color_duo_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/resistor-color-duo` directory.
+=end
+
+module ResistorColorDuo
+  def self.value(arr)
+    colors = {
+      black: 0,
+      brown: 1,
+      red: 2,
+      orange: 3,
+      yellow: 4,
+      green: 5,
+      blue: 6,
+      violet: 7,
+      gray: 8,
+      white: 9
+    }
+    "#{colors[arr[0].to_sym]}#{colors[arr[1].to_sym]}".to_i
+  end
+end
